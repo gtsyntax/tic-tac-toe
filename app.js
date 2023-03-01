fad9a7ebd53882e257e6a047832f4c68c43062d6
Greattony Oduche <greattony96@gmail.com>
2023-03-01 03:31:19 +0300

    refactor: game board object, new game object, and player object added

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
+const Player = () => {
+    const makeMove = (x, y) => {
+        GameBoard.addMark(x, y)
+    }
 
+    return {makeMove}
+}
+
+const GameBoard = (() => {
+    let curr = "O"
+    let gameBoard = [
+        ["-", "-", "-"],
+        ["-", "-", "-"],
+        ["-", "-", "-"]
+    ]
+
+    const displayBoard = () => {
+        for (let i = 0; i < gameBoard.length; i++) {
+            console.log(...gameBoard[i])
+        }
+    }
+
+    const addMark = (x, y) => {
+        if (gameBoard[x][y] === "-") {
+            gameBoard[x][y] = curr
+            curr = curr === "O" ? "X" : "O"
+            GameBoard.displayBoard()
+        } else {
+            console.log("Not a valid move")
+            GameBoard.displayBoard()
+        }
+    }
+
+    return {displayBoard, addMark}
+
+
+})();
+
+const NewGame = (() => {
+    console.log("Game Started!")
+    GameBoard.displayBoard()
+})();
